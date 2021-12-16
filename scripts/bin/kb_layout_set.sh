ebdaba59c7910055f5ee2b7a228b4d55bcd4af1a
dd <core@ddcveng.cc>
2021-12-16 12:32:34 +0100

    added more configs to source control

--- scripts/bin/kb_layout_set.sh
+++ scripts/bin/kb_layout_set.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/bash
 
+# should be run with X running
+# and dwmblocks started
+
 DELIM="|"
 SPACE=" "
 EMPTY="-"
 LAYOUT_SRC="$HOME/.local/share/layouts.dd"
 
-QUERY=$(awk "NR>2" $LAYOUT_SRC | dmenu -i -p "layout: ")
+if [ ! -f $LAYOUT_SRC ]; then
+    echo "Layout file not found!"
+    exit 1
+fi
+
+DEFAULT=$(grep '^*' $LAYOUT_SRC | tr -d '*')
+QUERY=$DEFAULT
+
+# todo: check value of $1
+if [ -z "$1" ]; then
+    QUERY=$(grep -v '^ *#' $LAYOUT_SRC | tr -d '*' | dmenu -i -p "layout: ")
+fi
+
 if [ -z $QUERY ]; then
     exit 1
 fi
@@ -27,3 +42,5 @@ fi
 
 setxkbmap $OPTIONS > /dev/null
 sigdwmblocks 2
+
+exit 0
